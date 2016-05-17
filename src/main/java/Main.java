a005d57e35aefb976991bdb4a62b5b39102c3e1b
Kaylleur <dev@thomas-brunet.fr>
2016-05-17 11:38:26 +0200

    init commit test with 2 conf file

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+/**
+ * Created by TO116676 on 17/05/2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Config conf = ConfigFactory.load("simple");
+        Config conf2 = ConfigFactory.load("complex");
+
+        System.out.println(conf.getString("simple-lib.foo"));
+        System.out.println(conf2.getString("test.foo"));
+
+
+    }
+}
